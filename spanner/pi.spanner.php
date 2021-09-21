904a2736b4879cdbfda1b5c3f90e82e4a48b15ec
Tom Rolfe <thomasrolfe@yahoo.co.uk>
2021-09-21 15:42:05 +0100

    Updated regex to include hyphens and numbers explicitly

--- spanner/pi.spanner.php
+++ spanner/pi.spanner.php
@@ -5,12 +5,13 @@ class Spanner
     public $return_data;
     public function __construct($str = '')
     {
-        $expression = "/(@)(.+)(@)(.*)(@@)/mU";
+        $expression = "/(@)([a-zA-Z0-9\-]+)(@)(.*)(@@)/mU";
         $replacement = '<span class="${2}">${4}</span>';
 
         $str = ($str == '') ? ee()->TMPL->tagdata : $str;
 
         $replaced = preg_replace($expression, $replacement, $str);
+        $matched = preg_match($expression, $str, $matches);
 
         $this->return_data = $replaced;
     }
